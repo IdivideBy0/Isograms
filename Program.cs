970fdb53ab20ac651ef3cd7777f9a51a863bc071
Michael Murray <num1stratplayer@yahoo.com>
2019-06-19 12:31:44 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Isograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsIsogram("moose");
+
+            IsIsogram("moOse", 1);
+        }
+
+
+        public static bool IsIsogram(string str, int myInt)
+        {
+        return str.ToLower().Distinct().Count()==str.Length;
+        }
+
+
+        public static bool IsIsogram(string str)
+        {
+            // Code on you crazy diamond!
+
+            int len = str.Length;
+            int cnt = 0;
+            char[] myChars = new char[len];
+
+            // poulate myChars array
+            for (int i = 0; i < len; i++)
+            {
+                myChars[i] = str.ToUpper()[i];
+
+                for (int j = 0; j < len; j++)
+                {
+                    if (myChars[i] == str.ToUpper()[j])
+                        cnt++;
+                }
+                    
+            }
+
+            if (cnt > len)
+                return false;
+            else
+                return true;
+        }
+
+
+    }
+}
